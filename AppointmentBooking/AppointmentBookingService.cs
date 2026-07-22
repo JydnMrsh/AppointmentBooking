fd2ab119902efe3e7abc81cba6b56803619aedd3
Jayden Marsh <skx5010@autuni.ac.nz>
2026-07-23 10:17:51 +1200

    Setup Skeleton Classes

---
+++ AppointmentBooking/AppointmentBookingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppointmentBooking
+{
+    public class AppointmentBookingService
+    {
+        public bool BookAppointment(AppointmentRequest request)
+        {
+            if (request.Doctor.AvailableSlots <= 0)
+            {
+                return false; // No available slots
+            }
+
+            request.Doctor.AvailableSlots--;
+            return true; // Appointment booked successfully
+        }
+    }
+}

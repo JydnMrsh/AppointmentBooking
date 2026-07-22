976b3a9c4b5f630a896a5df2d490f5e0cacf8784
Jayden Marsh <skx5010@autuni.ac.nz>
2026-07-23 10:45:53 +1200

    Improved Patient class

--- AppointmentBooking/Patient.cs
+++ AppointmentBooking/Patient.cs
@@ -6,7 +6,39 @@ namespace AppointmentBooking
 {
     public class Patient
     {
-        public string Id { get; set; }
-        public string FullName { get; set; }
+        public string Id { get; }
+        public string LegalName { get; }
+        public string PreferredName { get; }
+
+        // Get method for display name, which returns the preferred name if available, otherwise returns the legal name
+        public string DisplayName
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(PreferredName))
+                {
+                    return LegalName;
+                }
+
+                return PreferredName;
+            }
+        }
+
+        public Patient(string id, string legalName, string preferredName = "")
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentException("Patient ID is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(legalName))
+            {
+                throw new ArgumentException("Legal name is required.");
+            }
+
+            Id = id;
+            LegalName = legalName;
+            PreferredName = preferredName;
+        }
     }
 }
